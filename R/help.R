f78c9ca14b2db446dd9ad907c2db709616733900
Diego Diez <diego10ruiz@gmail.com>
2016-11-22 14:42:28 +0900

    Added Imports (for now all of them) and main package documentation.

---
+++ R/help.R
@@ -0,0 +1,11 @@
+#' A Simple Maze Plotting Tool
+#'
+#' A Simple Maze Plotting Tool
+#'
+#' @name maze-package
+#' @aliases maze
+#' @docType package
+#'
+#' @import ggplot2 igraph shiny shinyjs viridis RColorBrewer
+#'
+NULL
